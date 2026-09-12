ef9e2d755aebc378cbbc649f36854db106d5c621
uda224 <uda224@lehigh.edu>
2026-09-12 15:18:18 -0400

    working

--- fetch_news.py
+++ fetch_news.py
@@ -8,25 +8,38 @@ load_dotenv()  # reads .env and loads its variables into the environment
 
 newsapi = NewsApiClient(os.getenv("NEWSAPI_KEY"))
 
-countries = ['us', 'india', 'europe', 'china']
+countries = ['us', 'in', 'cn', 'ca']
 
-categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+categories = ['business', 'entertainment']
+# all categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
 
 # create array of user countries
 # array of user categories
 # put forloop to get all possible responses
 # add responses in a data structure
 
-top_headlines_response = newsapi.get_top_headlines(
-    category='business',
-    language='en',
-    country='us'
-)
+# ----- working section
+# top_headlines_response = newsapi.get_top_headlines(
+#     category='business',
+#     language='en',
+#     country='us'
+# )
 
-#print(top_headlines_response["articles"][0])
+# for article in top_headlines_response["articles"]:
+#     print(article["title"], article["content"], article["url"])
 
-for article in top_headlines_response["articles"]:
-    print(article["title"], article["content"], article["url"])
+for current_country in countries:
+    for current_category in categories:
+        top_headlines_response = newsapi.get_top_headlines(
+            category=current_category,
+            language='en',
+            country=current_country
+        )
+
+
+
+
+
 
 #   article_id: auto increment
 #    title: Mapped[str] = title
